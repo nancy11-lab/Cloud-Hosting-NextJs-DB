4989fc814dd3f84779005c6cfefbc2625749c29a
Nancy <nancy.elsayed949@gmail.com>
2025-12-04 11:21:38 +0200

    Domain added

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
 
 export const ARTICLE_PER_PAGE = 6;
-export const DOMAIN = "http://localhost:3000";
+// export const DOMAIN = "http://localhost:3000";
+export const DOMAIN = "https://cloud-hosting-next-js-db.vercel.app";
