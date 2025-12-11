65a02b5f6859118c59e33eb4c8d7f58c96a53967
Nancy <nancy.elsayed949@gmail.com>
2025-12-11 23:14:56 +0200

    Add function to fetch user info from API endpoint

---
+++ src/apiCalls/userProfileApiCall.ts
@@ -0,0 +1,19 @@
+import { User } from "@/generated/prisma/client";
+import { DOMAIN } from "@/utils/constants";
+
+export async function getUserProfile(
+  token: string,
+  userId: number
+): Promise<User> {
+  const response = await fetch(`${DOMAIN}/api/users/profile/${userId}`, {
+    headers: {
+      cookie: `jwtToken=${token}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to get user profile");
+  }
+
+  return response.json();
+}
