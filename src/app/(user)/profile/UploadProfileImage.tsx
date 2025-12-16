233e921d94f85b090cf54aacfdb26225ca210d17
Nancy <nancy.elsayed949@gmail.com>
2025-12-16 05:53:55 +0200

    fix: use CLOUDINARY_UPLOAD_PRESET from .env in profile image upload

--- src/app/(user)/profile/UploadProfileImage.tsx
+++ src/app/(user)/profile/UploadProfileImage.tsx
@@ -30,7 +30,9 @@ const UploadProfileImage = ({ userId }: UploadProfileImageProps) => {
     try {
       const formData = new FormData();
       formData.append("file", file);
-      formData.append("upload_preset", "profile_images");
+      // formData.append("upload_preset", "profile_images");
+      const preset : string = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET!
+      formData.append("upload_preset", preset);
       const cloudRes = await axios.post(
         `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
         formData
