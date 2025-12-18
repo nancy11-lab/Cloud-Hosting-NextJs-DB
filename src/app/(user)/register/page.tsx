8e4a97976ed1187f9a07cc5863a5908df59efa25
Nancy <nancy.elsayed949@gmail.com>
2025-12-18 21:20:51 +0200

    style: improve pages UI

--- src/app/(user)/register/page.tsx
+++ src/app/(user)/register/page.tsx
@@ -2,9 +2,9 @@ import RegisterForm from "./RegisterForm";
 
 const RegisterPage =  () => {
   return (
-    <section className="fix-height container m-auto px-7 flex items-center justify-center">
+    <section className="fix-height container m-auto px-3 sm:px-7 flex items-center justify-center">
       <div className="bg-white m-auto p-5 rounded-lg  w-full md:w-2/3">
-        <h1 className="text-3xl font-bold text-gray-800 mb-5">
+        <h1 className="text-2xl  md:text-3xl font-bold text-gray-800 mb-5">
           Create New Account
         </h1>
         <RegisterForm />
