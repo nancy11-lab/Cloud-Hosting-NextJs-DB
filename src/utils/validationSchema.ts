27b2ce06fd472c568b2468f29e906c86b1bb3608
Nancy <nancy.elsayed949@gmail.com>
2025-12-18 22:05:26 +0200

    feat(validation): improve password rules and confirm password to update profile schema

--- src/utils/validationSchema.ts
+++ src/utils/validationSchema.ts
@@ -18,25 +18,53 @@ export const createArticleSchema = z.object({
 export const registerSchema = z.object({
   username: z
     .string()
-    .min(2)
-    .max(100)
+    .nonempty({ message: "username is required" })
+    .min(2, { message: "username should be at least 2 characters" })
+    .max(100, { message: "username should be less than 100 characters" })
     .refine((val) => /^[a-zA-Z]{2}/.test(val), {
       message: "username must start with at least 2 letters",
-    }), //.optional(),
-  email: z.string().min(3).max(200).email(),
-  password: z.string().min(6),
+    }),
+  email: z
+    .string()
+    .nonempty({ message: "Email is required" })
+    .min(3, { message: "Email should be at least 3 characters long" })
+    .max(200)
+    .email({ message: "invalid email format" }),
+  password: z
+    .string()
+    .nonempty({ message: "Password is required" })
+    .min(6, { message: "password must be at least 6 characters" })
+    .max(30, { message: "password must be less than 30 characters" })
+    .regex(
+      /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&#])/,
+      "Password  must be at least 6 character and include a capital letter , a lowercase letter , a number, and spicial character (@, # , $ , etc.)"
+    ),
 });
 
 // Login Schema
 export const loginSchema = z.object({
-  email: z.string().min(3).max(200).email(),
-  password: z.string().min(6),
+  email: z
+    .string()
+    .nonempty({ message: "Email is required" })
+    .min(3, { message: "Email should be at least 3 characters long" })
+    .max(200)
+    .email({ message: "invalid email format" }),
+  password: z
+    .string()
+    .nonempty({ message: "Password is required" })
+    .min(6, { message: "password must be at least 6 characters" })
+    .max(30, { message: "password must be less than 30 characters" })
+    .regex(
+      /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&#])/,
+      "Password  must be at least 6 character and include a capital letter , a lowercase letter , a number, and spicial character (@, # , $ , etc.)"
+    ),
 });
 
 //update user profile schema
 export const updateUserSchema = z.object({
   username: z
     .string()
+    .nonempty({ message: "username is required" })
     .min(2, { message: "username must be at least 2 characters" })
     .max(100, { message: "username should be less than 100 characters" })
     .refine((val) => /^[a-zA-Z]{2}/.test(val), {
@@ -53,31 +81,60 @@ export const updateUserSchema = z.object({
   password: z
     .string()
     .min(6, { message: "password must be at least 6 characters" })
+    .max(30, { message: "password must be less than 30 characters" })
     .optional(),
   image: z.string().nullable().optional(),
 });
-//update user profile schema
-export const updateUserSchemaClient = z.object({
-  username: z
-    .string()
-    .min(2, { message: "username must be at least 2 characters" })
-    .max(100, { message: "username should be less than 100 characters" })
-    .refine((val) => /^[a-zA-Z]{2}/.test(val), {
-      message: "username must start with at least 2 letters",
-    })
-    .optional(),
-  email: z
-    .string()
-    .nonempty({ message: "Email is required" })
-    .min(3, { message: "Email should be at least 3 characters long" })
-    .max(200)
-    .email({ message: "invalid email format" })
-    .optional(),
-  password: z
-    .string()
-    .min(6, { message: "password must be at least 6 characters" })
-    .optional(),
-});
+//update user profile schema in client
+const passwordSchema = z
+  .string()
+  .min(6, { message: "password must be at least 6 characters" })
+  .max(30, { message: "password must be less than 30 characters" })
+  .regex(
+    /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&#])/,
+    "Password  must be at least 6 character and include a capital letter , a lowercase letter , a number, and spicial character (@, # , $ , etc.)"
+  )
+  // .or(z.literal(""))
+  .optional();
+
+export const updateUserSchemaClient = z
+  .object({
+    username: z
+      .string()
+      .nonempty({ message: "username is required" })
+      .min(2, { message: "username must be at least 2 characters" })
+      .max(100, { message: "username should be less than 100 characters" })
+      .refine((val) => /^[a-zA-Z]{2}/.test(val), {
+        message: "username must start with at least 2 letters",
+      })
+      .optional(),
+    email: z
+      .string()
+      .nonempty({ message: "Email is required" })
+      .min(3, { message: "Email should be at least 3 characters long" })
+      .max(200)
+      .email({ message: "invalid email format" })
+      .optional(),
+    password: passwordSchema,
+    confirmPassword: z.string().optional(),
+  })
+  .superRefine((data, ctx) => {
+    if (data.password && data.password.trim() !== "") {
+      if (!data.confirmPassword || data.confirmPassword.trim() === "") {
+        ctx.addIssue({
+          path: ["confirmPassword"],
+          message: "Confirm password is required",
+          code: "custom",
+        });
+      } else if (data.password !== data.confirmPassword) {
+        ctx.addIssue({
+          path: ["confirmPassword"],
+          message: "Password and confirm password do not match",
+          code: "custom",
+        });
+      }
+    }
+  });
 
 // create comment schema
 export const createCommentSchema = z.object({
